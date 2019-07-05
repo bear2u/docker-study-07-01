d4056e72429aeebade468d43373d7fe9481e3ecd
김태현 <bear2u@naver.com>
2019-07-05 21:29:41 +0900

    connect mongodb

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -21,6 +22,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+var db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', function(){    
+    console.log("Connected to mongod server");
+});
+
+mongoose.connect('mongodb://app-mongo/study');
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
